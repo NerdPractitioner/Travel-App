2abc233796aa20288bd2fe8f844fc8da8b558b2e
Matthew Holmes <mlholmes91@gmail.com>
2019-09-26 22:59:59 +0100

    build initial profile app

---
+++ UserProfiles/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class UserprofilesConfig(AppConfig):
+    name = 'UserProfiles'
+
+  #  def ready(self):
+    #    import UserProfiles.signals
---
+++ UserProfiles/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'UserProfiles/register.html', {'form': form})
+
+
+
+@login_required
+def profile(request):
+    return render(request, 'UserProfiles/profile.html')
