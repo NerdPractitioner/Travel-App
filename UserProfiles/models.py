3107e448012a7827bc065aa7ffb7062fe4f8c678
Matthew Holmes <mlholmes91@gmail.com>
2019-10-16 16:06:28 +0100

    Debug unique id issues

--- UserProfiles/models.py
+++ UserProfiles/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+
 
 class Profile(models.Model):
     TRAVEL_STYLE = (
@@ -22,15 +21,7 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
 
-
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
 
 
 
