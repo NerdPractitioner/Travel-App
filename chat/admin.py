7324f5fb85736976679c76f8fbc0444e589cad40
Matthew Holmes <mlholmes91@gmail.com>
2019-11-03 21:27:16 +0000

    Improve chat to allow messages to be saved

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Message
+
+admin.site.register(Message)
+
+
+"""
+from django.contrib import admin
+
+from .models import Contact, Chat, Message
+
+admin.site.register(Chat)
+admin.site.register(Contact)
+admin.site.register(Message)
+"""
---
+++ chat/models.py
@@ -0,0 +1,56 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Message(models.Model):
+    author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    def last_10_messages():
+        return Message.objects.order_by('-timestamp').all()[:10]
+
+
+"""
+
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Contact(models.Model):
+    user = models.ForeignKey(
+        User, related_name='friends', on_delete=models.CASCADE)
+    friends = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Message(models.Model):
+    contact = models.ForeignKey(
+        Contact, related_name='messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.contact.user.username
+
+    def last_10_messages(self):
+        return Message.objects.order_by('-timestamp').all()[:10]
+
+
+class Chat(models.Model):
+    participants = models.ManyToManyField(
+        Contact, related_name='chats', blank=True)
+    messages = models.ManyToManyField(Message, blank=True)
+
+    def __str__(self):
+        return "{}".format(self.pk)
+"""
