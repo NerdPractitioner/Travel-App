ce15dfd8063ca79a5e6c0e74e3abd5d5cc3c32e3
Matthew Holmes <mlholmes91@gmail.com>
2019-12-17 15:07:20 +0000

    comment sqlite db

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -94,14 +94,14 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
+#if DEBUG:
+ #   DATABASES = {
+  #      'default': {
+   #         'ENGINE': 'django.db.backends.sqlite3',
+    #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     #   }
+#    }
+#else:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
